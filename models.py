e778232678d806020208cf84d170bcdd11c0437e
Hanlei Zhang <zhanghanlei98@gmail.com>
2020-12-14 11:36:16 +0800

    Add files via upload

---
+++ models.py
@@ -0,0 +1,28 @@
+from utils import *
+        
+class BertForModel(BertPreTrainedModel):
+    def __init__(self,config,num_labels):
+        super(BertForModel, self).__init__(config)
+        self.num_labels = num_labels
+        self.bert = BertModel(config)
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.activation = nn.Tanh()
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, num_labels)
+        self.apply(self.init_bert_weights)
+
+    def forward(self, input_ids = None, token_type_ids = None, attention_mask=None , labels = None, mode = None, centroids = None, labeled = False, feature_ext = False):
+
+        encoded_layer_12, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers = False)
+        pooled_output = self.dense(encoded_layer_12.mean(dim = 1))
+        pooled_output = self.activation(pooled_output)
+        pooled_output = self.dropout(pooled_output)        
+        logits = self.classifier(pooled_output)
+#         logits_norm = F.normalize(logits, p=2, dim=1)
+        if feature_ext:
+            return pooled_output
+        elif mode == 'train':
+            loss = nn.CrossEntropyLoss()(logits, labels)
+            return loss
+        else:
+            return pooled_output, logits
---
+++ scripts/run_k.sh
@@ -0,0 +1,44 @@
+
+#!/usr/bin bash
+
+for s in 0 1 2 3 4 5 6 7 8 9
+do 
+    for c in 2 3 4
+    do
+        
+        python aba.py \
+            --dataset stackoverflow \
+            --known_cls_ratio 0.75 \
+            --cluster_num_factor $c \
+            --seed $s \
+            --num_train_epochs 100 \
+            --num_pretrain_epochs 100 \
+            --lr 5e-5 \
+            --lr_pre 5e-5 \
+            --gpu_id 0 \
+            --save_results \
+            --freeze_bert_parameters \
+            --pretrain
+    done
+done
+
+# for s in 0
+# do 
+#     for c in 2
+#     do
+        
+#         python DeepAligned.py \
+#             --dataset banking \
+#             --known_cls_ratio 0.75 \
+#             --cluster_num_factor $c \
+#             --seed $s \
+#             --num_train_epochs 100 \
+#             --num_pretrain_epochs 100 \
+#             --lr 5e-5 \
+#             --lr_pre 5e-5 \
+#             --gpu_id 1 \
+#             --save_results \
+#             --freeze_bert_parameters
+            
+#     done
+# done
